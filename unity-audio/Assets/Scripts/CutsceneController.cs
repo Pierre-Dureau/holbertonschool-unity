eb900cebc2430aa7c69a2d605f831518434ed098
Dureau <dureaupierre53@gmail.com>
2023-11-14 11:28:24 +0100

    Anim intro for each level

--- unity-audio/Assets/Scripts/CutsceneController.cs
+++ unity-audio/Assets/Scripts/CutsceneController.cs
@@ -6,18 +6,19 @@ public class CutsceneController : MonoBehaviour
     [SerializeField] private GameObject mainCamera;
     [SerializeField] private PlayerController playerController;
     [SerializeField] private GameObject timerCanvas;
+    [SerializeField] private Animator anim;
+    [SerializeField] private string AnimName;
 
     void Start()
     {
         mainCamera.SetActive(false);
         playerController.enabled = false;
         timerCanvas.SetActive(false);
-        StartCoroutine(GameStart());
+        anim.Play(AnimName);
     }
 
-    IEnumerator GameStart()
+    public void GameStart()
     {
-        yield return new WaitForSeconds(2.1f);
         gameObject.SetActive(false);
         mainCamera.SetActive(true);
         playerController.enabled = true;
