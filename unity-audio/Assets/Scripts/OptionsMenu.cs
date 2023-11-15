1e5967607488034dc209b82c5613a5c1ecb9e968
Dureau <dureaupierre53@gmail.com>
2023-11-15 10:27:10 +0100

    edit option

--- unity-audio/Assets/Scripts/OptionsMenu.cs
+++ unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,9 +6,14 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] private Toggle yAxis;
+    [SerializeField] private Slider BGMSlider;
+    [SerializeField] private Slider SFXSlider;
 
     private void Start()
     {
+        BGMSlider.value = PlayerPrefs.GetFloat("BGMSliderValue");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXSliderValue");
+
         if (PlayerPrefs.GetInt("yAxis") == 1)
             yAxis.isOn = true;
         else
@@ -21,6 +27,9 @@ public class OptionsMenu : MonoBehaviour
 
     public void Apply()
     {
+        PlayerPrefs.SetFloat("BGMSliderValue", BGMSlider.value);
+        PlayerPrefs.SetFloat("SFXSliderValue", SFXSlider.value);
+
         if (yAxis.isOn == true)
             PlayerPrefs.SetInt("yAxis", 1);
         else
